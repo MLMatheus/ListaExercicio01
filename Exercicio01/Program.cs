7c09701a726f9a67372d17768093cf36d9317d54
MLMatheus <61673626+MLMatheus@users.noreply.github.com>
2021-10-19 12:10:36 -0300

    Primeiro exercicio concluido

--- Exercicio01/Program.cs
+++ Exercicio01/Program.cs
@@ -6,7 +6,24 @@ namespace Exercicio01
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Informe a primeira nota: ");
+            double n1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Informe a segunda nota: ");
+            double n2 = Convert.ToDouble(Console.ReadLine());
+
+            double media = (n1 + n2)/2;
+            if(media == 10)
+            {
+                Console.WriteLine("Aprovado com distinção");
+            }
+            else if(media >= 7)
+            {
+                Console.WriteLine("Aprovado");
+            }
+            else
+            {
+                Console.WriteLine("Reprovado");
+            }
         }
     }
 }
